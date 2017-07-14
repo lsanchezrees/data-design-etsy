98b383cb06dc5733c4c2a51ffdbf1de54e3b9927
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-14 09:18:30 -0600

    Created new index.php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<p>This document contains all the information for the data design project.</p>
+
+	</body>
+</html>
