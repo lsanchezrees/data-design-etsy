371bbadc8fc088896264edd339ccba6cf3a1ab4f
Leonora Sanchez-Rees <lsanchezrees@cnm.edu>
2017-07-19 16:54:47 -0600

    created sql file

---
+++ sql/dml-example.sql
@@ -0,0 +1,80 @@
+-- can run these queries piece by piece
+-- insert three new sellers
+INSERT INTO seller
+	(sellerName,
+	sellerStoreName,
+	sellerLocation,
+	sellerPhone,
+	sellerEmail,
+	sellerHash,
+	sellerSalt )
+	VALUES
+	(
+	"Sally Merlot",
+	"Cute Things I Made",
+	"Santa Fe, NM",
+	"505-222-3333",
+	"sallymerlot@yahoo.com",
+	"JDODJWdjw98n2uf2kj30282k928fnk",
+	"2j33ijth2o30IHi"
+	);
+
+	INSERT INTO seller
+	(sellerName,
+	sellerStoreName,
+	sellerLocation,
+	sellerPhone,
+	sellerEmail,
+	sellerHash,
+	sellerSalt )
+	VALUES
+	(
+	"Anna Banana",
+	"Another Cute Store",
+	"Albuquerque, NM",
+	"505-111-2222",
+	"annabanana@gmail.com",
+	"DDJOENKDNw990jjdknjkK20dlnd",
+	"JDOFjk2ji89"
+	);
+
+INSERT INTO seller
+	(sellerName,
+	sellerStoreName,
+	sellerLocation,
+	sellerPhone,
+	sellerEmail,
+	sellerHash,
+	sellerSalt )
+	VALUES
+	(
+	"Jane Plain",
+	"Kitsch and More",
+	"Oklahome, OK",
+	"505-555-9898",
+	"pjane@yahoo.com",
+	"JDOn89nlkn2NKL02nklnlA",
+	"j29nkNKN128398u"
+	);
+
+-- insert items for the first two sellers
+
+
+-- insert a photo for each item, and insert two photos for
+-- at least one item
+
+
+--  update a seller
+
+--  update an item
+
+
+--delete an item
+
+
+-- delete
+
+
+-- select statements
+
+-- advanced queries
